18a7f72972723805aba24ee23e61be7b768ee190
unparalleled-js <yingthi@live.com>
2022-06-02 11:41:45 -0500

    feat: reset config file

--- scripts/reset.py
+++ scripts/reset.py
@@ -6,9 +6,7 @@ import click
 from ape import project
 
 
-@click.command("reset")
-def cli():
-    click.echo("Resetting...")
+def delete_directories():
 
     directories = [
         project.contracts_folder,
@@ -28,3 +26,18 @@ def cli():
                 file_path.unlink()
             elif file_path.is_dir():
                 shutil.rmtree(file_path)
+
+
+def reset_config_file():
+    config_file = project.path / "ape-config.yaml"
+    if config_file.is_file():
+        config_file.unlink()
+
+    config_file.touch()
+
+
+@click.command("reset")
+def cli():
+    click.echo("Resetting...")
+    delete_directories()
+    reset_config_file()
